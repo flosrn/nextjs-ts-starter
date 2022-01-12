a5e087a86e092af5babe240e4acbbfe49b40fc25
Flosrn <flosrn.dev@gmail.com>
2022-01-13 00:11:31 +0100

    :art: feat(helper.js): turn function to const

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -7,13 +7,13 @@ type OpenGraphType = {
 // !STARTERCONF This OG is generated from https://github.com/theodorusclarence/og
 // Please clone them and self-host if your site is going to be visited by many people.
 // Then change the url and the default logo.
-export function openGraph({
+const openGraph = ({
   siteName,
   templateTitle,
   description,
   // !STARTERCONF Or, you can use my server with your own logo.
   logo = 'https://og.thcl.dev/images/logo.jpg',
-}: OpenGraphType): string {
+}: OpenGraphType): string => {
   const ogLogo = encodeURIComponent(logo);
   const ogSiteName = encodeURIComponent(siteName.trim());
   const ogTemplateTitle = templateTitle
@@ -24,4 +24,6 @@ export function openGraph({
   return `https://og.thcl.dev/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
     ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
   }`;
-}
+};
+
+export default openGraph;
